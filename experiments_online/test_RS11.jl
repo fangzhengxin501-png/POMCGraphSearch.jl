fa934e177bfd140903c2259c19b4904f1d61a032
Zhengxin Fang <zhengxinfang@FF-Mac.local>
2026-09-12 14:07:52 +0100

    update rs11, with smaller vmdp epsilon and search depth

--- experiments_online/test_RS11.jl
+++ experiments_online/test_RS11.jl
@@ -11,13 +11,14 @@ Random.seed!(1)
 pomdp = RockSamplePOMDP(11,11)
 
 pomcgs = SolverPOMCGS(pomdp;
-    max_b_gap = 0.3, #offset 0.2, 
-    max_search_depth = 40,
+    max_b_gap = 0.2, #offset 0.2, 
+    max_search_depth = 50, #original 40 in offline
     num_sim_per_sa = 20,
-    nb_particles = 10000  #given in table 2
+    nb_particles = 10000,  #given in table 2, offset 10000
+    epsilon_VMDP = 0.01 #offset is 0.1
 )
 
-planning_time = 3.0 #offset 2.0
+planning_time = 6.0 #offset 2.0
 max_depth = 100
 nb_runs = 100 
 
@@ -33,9 +34,9 @@ end
 df = DataFrame(run_id = 1:nb_runs, return_value = results)
 #CSV.write("pomcgs_results_RS1111.csv", df)
 timestamp = Dates.format(now(), "yyyymmdd_HHMMSS")
-CSV.write("on_off_compare_data/RS11_table2_$(timestamp).csv", df)
+CSV.write("on_off_compare_data/RS11_6s_0.2_setted_$(timestamp)_$(mean(results)).csv", df)
 
 
 println("\nTotal return: $(sum(results))")
 println("Average return: $(mean(results))")
-println("Results saved to RS11_table2_$(timestamp).csv")
+println("Results saved to RS11_6s_0.2_setted_$(timestamp).csv")
